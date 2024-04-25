4bef04f8391394c81407c9e0ea96429ca9ad93f4
greidimar <greidi123@gmail.com>
2024-04-25 20:55:38 -0300

    Feito

---
+++ assets/js/cadastrar.js
@@ -0,0 +1,34 @@
+
+document.querySelector("#botao-cadastrar").addEventListener("click", (e) => {
+    e.preventDefault()
+    //console.log("cadastrar...")
+
+   const form = document.querySelector("form")
+   //json origem
+   const tarefa = {
+    titulo: form.titulo.value,
+    descricao: form.descricao.value,
+    pontos: form.pontos.value
+   }
+
+   //console.log(form.titulo.value)
+
+
+  salvar(tarefa)
+
+})
+
+function salvar(tarefa){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    window.location = "index.html"
+}
+
+
+
+
+
+
+
---
+++ assets/js/carregar.js
@@ -0,0 +1,36 @@
+
+
+const tarefas = JSON.parse(localStorage.getItem("tarefas"))
+
+
+tarefas.forEach( tarefa =>  
+//console.log(tarefa)
+card(tarefa)
+
+)
+
+function card(tarefa)
+{
+    //template literals
+const content = `<div class="nes-container with-title is-centered">
+<p class="title">${tarefa.titulo}</p>
+<p> ${tarefa.descricao}</p>
+<a href="#" class="nes-badge is-icon">
+    <span class="is-sucess"> ${tarefa.pontos}</span>
+    <span class="is-primary">+150</span>
+</a>
+
+<progress class="nes-progress is-sucess" value="10" max="100"> </progress>
+
+<button type="button" class="nes-btn is-primary">-</button>
+<button type="button" class="nes-btn is-error">Apagar</button>
+<button type="button" class="nes-btn is-primary">+</button>
+</div>`
+
+const card = document.createElement("div")
+card.innerHTML = content
+
+document.querySelector("#lista-de-tarefas").appendChild(card)
+
+}
+    
